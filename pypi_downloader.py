405f31662be60862dc8c9dc0f79ecc75cf61e80d
Anton Demkin <antondemkin@yandex.ru>
2022-03-02 22:37:31 +0200

    Split pipeline into smaller pieces, fix logging

--- pypi_downloader.py
+++ pypi_downloader.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
+from asyncio import Queue
 from concurrent.futures import ThreadPoolExecutor
+from contextlib import suppress
 from os import listdir
 from os import makedirs
 from pathlib import Path
@@ -8,13 +10,19 @@ import asyncio
 import logging
 import re
 
+from aiofiles.os import rename as aiorename
+from aiofiles.tempfile import NamedTemporaryFile as aioNamedTemporaryFile
 from aiohttp import ClientSession
-import aiofiles
 
 PYPI_SIMPLE = "https://pypi.python.org/simple"
 PYPI_JSON_TEMPALTE = "https://pypi.python.org/pypi/{package_name}/json"
 
+
 logger = logging.getLogger("pypi downloader")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s %(asctime)-10s: %(message)s',
+)
 
 
 class Paths(NamedTuple):
@@ -69,9 +77,9 @@ def get_file_path_from_file_name(file_name: str, paths: Paths) -> Path:
     return paths.wheel / file_name
 
 
-async def get_package_names(
+async def names_fetcher(
         session: ClientSession,
-        names: asyncio.Queue,
+        names_queue: Queue,
 ) -> None:
     logger.info("Started")
     async with session.get(PYPI_SIMPLE) as resp:
@@ -81,17 +89,17 @@ async def get_package_names(
         content = await resp.text()
     for match in re.finditer(r'\s+<a href="([^"]+)">([^<]+)</a>', content):
         _, name = match.groups()
-        await names.put(name)
+        await names_queue.put(name)
     logger.info("got all packages names")
 
 
-async def get_package_uris(
+async def uris_fetcher(
         session: ClientSession,
-        names: asyncio.Queue,
-        uris: asyncio.Queue,
+        names_queue: Queue,
+        uris_to_check_queue: Queue,
 ) -> None:
     while True:
-        name = await names.get()
+        name = await names_queue.get()
         logger.debug("package: %s taken", name)
         package_uri = PYPI_JSON_TEMPALTE.format(package_name=name)
         async with session.get(package_uri) as resp:
@@ -103,56 +111,106 @@ async def get_package_uris(
                 )
                 continue
             package_data = await resp.json()
-        try:
-            await uris.put(package_data['urls'][0]['url'])
-        except (KeyError, IndexError):
-            pass
+        with suppress(KeyError, IndexError):
+            await uris_to_check_queue.put(package_data['urls'][0]['url'])
         for releases in package_data['releases'].values():
             for release in releases:
-                await uris.put(release['url'])
+                await uris_to_check_queue.put(release['url'])
         logger.debug("package: %s done", name)
 
 
-async def download_uri(
+async def uri_checker(
         number: int,
-        session: ClientSession,
         paths: Paths,
-        uris: asyncio.Queue,
-        fs_semaphore: asyncio.Semaphore,
+        uris_to_check_queue: Queue,
+        uris_to_download_queue: Queue,
 ) -> None:
     while True:
-        uri = await uris.get()
-        logger.debug("uri-%d: %r taken", number, uri)
+        uri = await uris_to_check_queue.get()
+        logger.debug("checker-%d: %r taken", number, uri)
         file_name = get_file_name_from_uri(uri)
         file_path = str(get_file_path_from_file_name(file_name, paths))
         if file_path in paths.known_paths:
-            logger.debug("uri-%d: %r already done", number, uri)
+            logger.debug("checker-%d: %r already done", number, uri)
             continue
+        await uris_to_download_queue.put((uri, file_path))
+        logger.debug("checker-%d: %r done", number, uri)
+
+
+async def uri_downloader(
+        number: int,
+        session: ClientSession,
+        uris_to_download_queue: Queue,
+        files_to_write_queue: Queue,
+        timeout: int = 15,
+) -> None:
+    while True:
+        uri, file_path = await uris_to_download_queue.get()
+        logger.debug("downloader-%d: %r taken", number, uri)
         async with session.get(uri) as resp:
             if resp.status != 200:
-                logger.error("uri-%d: %r status: %d", number, uri, resp.status)
+                logger.error(
+                    "downloader-%d: %r status: %d",
+                    number,
+                    uri,
+                    resp.status,
+                )
+                await uris_to_download_queue.put((uri, file_path))
+                await asyncio.sleep(timeout)
                 continue
             content_raw = await resp.read()
-        async with fs_semaphore:
-            async with aiofiles.open(file_path, "wb") as fd:
+        await files_to_write_queue.put((file_path, content_raw))
+        logger.debug("downloader-%d: %r done", number, uri)
+
+
+async def file_writer(
+        number: int,
+        files_to_write_queue: Queue,
+        paths: Paths,
+        timeout: int = 15,
+) -> None:
+    while True:
+        file_path, content_raw = await files_to_write_queue.get()
+        logger.debug("writer-%d: %r taken", number, file_path)
+        try:
+            async with aioNamedTemporaryFile(delete=False) as fd:
                 await fd.write(content_raw)
+                await fd.flush()
+            await aiorename(fd.name, file_path)
+        except OSError as err:
+            logger.exception("writer-%d: %r", number, err)
+            await files_to_write_queue.put((file_path, content_raw))
+            await asyncio.sleep(timeout)
+            continue
         paths.add_path(file_path)
-        logger.debug("uri-%d: %r done", number, uri)
+        logger.debug("writer-%d: %r done", number, file_path)
 
 
 async def queue_watcher(
-        names: asyncio.Queue,
-        uris: asyncio.Queue,
-        download_streams_count: int,
+        names_queue: Queue,
+        uris_to_check_queue: Queue,
+        uris_to_download_queue: Queue,
+        files_to_write_queue: Queue,
+        checkers_count: int,
+        downloaders_count: int,
+        writers_count: int,
+        timeout: int = 5
 ) -> None:
+    workers_message = (
+        f"Checkers: {checkers_count} "
+        f"Downlaoders: {downloaders_count} "
+        f"Writers: {writers_count}"
+    )
     while True:
         logger.info(
-            "Queue names: %d uris: %d streams: %d",
-            names.qsize(),
-            uris.qsize(),
-            download_streams_count,
+            "names %d > tocheck %d > todownload %d > towrite %d",
+            names_queue.qsize(),
+            uris_to_check_queue.qsize(),
+            uris_to_download_queue.qsize(),
+            files_to_write_queue.qsize(),
         )
-        await asyncio.sleep(5)
+        logger.info(workers_message)
+        await asyncio.sleep(timeout)
 
 
 async def main(
@@ -160,29 +218,61 @@ async def main(
         download_streams_count: int,
         show_queue: bool,
 ) -> None:
-    names_queue: asyncio.Queue = asyncio.Queue(maxsize=30)
-    uris_queue: asyncio.Queue = asyncio.Queue()
+    checkers_count = 30  # should be enought
+    downloaders_count = download_streams_count
+    writers_count = 10  # check ulimit -n
     paths.fill_known_paths()
-    fs_semaphore = asyncio.Semaphore(value=64)  # ulimit -n // 4
     logger.info(
         "found %d files in %s",
         len(paths.known_paths),
         str(paths.base),
     )
+    names_queue: Queue = Queue(maxsize=10)
+    uris_to_check_queue: Queue = Queue()
+    uris_to_download_queue: Queue = Queue(maxsize=downloaders_count * 2)
+    files_to_write_queue: Queue = Queue(maxsize=writers_count * 3)
     async with ClientSession() as session:
         tasks = []
-        tasks.append(get_package_uris(session, names_queue, uris_queue))
-        for i in range(download_streams_count):
-            tasks.append(download_uri(
-                i,
-                session,
-                paths,
-                uris_queue,
-                fs_semaphore,
+        tasks.append(names_fetcher(
+            session=session,
+            names_queue=names_queue,
+        ))
+        tasks.append(uris_fetcher(
+            session=session,
+            names_queue=names_queue,
+            uris_to_check_queue=uris_to_check_queue,
+        ))
+        for i in range(checkers_count):
+            tasks.append(uri_checker(
+                number=i,
+                paths=paths,
+                uris_to_check_queue=uris_to_check_queue,
+                uris_to_download_queue=uris_to_download_queue,
+            ))
+        for i in range(downloaders_count):
+            tasks.append(uri_downloader(
+                number=i,
+                session=session,
+                uris_to_download_queue=uris_to_download_queue,
+                files_to_write_queue=files_to_write_queue,
+            ))
+        for i in range(writers_count):
+            tasks.append(file_writer(
+                number=i,
+                files_to_write_queue=files_to_write_queue,
+                paths=paths,
             ))
         if show_queue:
             tasks.append(
-                queue_watcher(names_queue, uris_queue, download_streams_count),
+                queue_watcher(
+                    names_queue=names_queue,
+                    uris_to_check_queue=uris_to_check_queue,
+                    uris_to_download_queue=uris_to_download_queue,
+                    files_to_write_queue=files_to_write_queue,
+                    checkers_count=checkers_count,
+                    downloaders_count=downloaders_count,
+                    writers_count=writers_count,
+                ),
             )
         await asyncio.gather(*tasks)
 
@@ -213,18 +303,13 @@ def prepare() -> None:
         help="Print queue size info",
     )
     parser.add_argument(
-        "--loglevel",
-        type=str,
-        default="info",
-        metavar="INFO",
-        help="Logger level info",
+        "--debug",
+        action="store_true",
+        help="Set logging level to debug",
     )
     args = parser.parse_args()
-    logging.basicConfig(
-        level=logging.getLevelName(args.loglevel.upper()),
-        format='%(levelname)s %(asctime)-10s: %(message)s',
-    )
     paths = Paths.create(args.path)
+    logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(exception_handler)
     with ThreadPoolExecutor() as executor:
